2ed9cd43ad0b027322fb7588f9c537909b98c865
J. Roberto <jose.gonzalez@gonet.us>
2019-02-18 23:18:31 -0600

    Primer Commit

---
+++ GonetApp/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  GonetApp
+//
+//  Created by Jose González on 2/18/19.
+//  Copyright © 2019 Gonet. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
