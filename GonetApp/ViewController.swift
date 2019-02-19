d9c2cc164f4c6b741477e1bc0c92c4e2be1cd944
J. Roberto <jose.gonzalez@gonet.us>
2019-02-18 22:55:28 -0600

    Initial Commit

---
+++ GonetApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GonetApp
+//
+//  Created by Jose González on 2/18/19.
+//  Copyright © 2019 Gonet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
